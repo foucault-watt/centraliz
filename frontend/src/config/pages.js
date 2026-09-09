77aeac34ac1d6e1b2aa7e302a7ee5bddd7bedc8c
Foucault Wattinne <80125002+foucault-watt@users.noreply.github.com>
2026-09-09 14:53:38 +0200

    Désactiver temporairement les onglets Notes et Pokémon

--- frontend/src/config/pages.js
+++ frontend/src/config/pages.js
@@ -9,11 +9,11 @@ import {
 } from "lucide-react";
 
 export const pagesConfig = [
-  { id: "notes", label: "Notes", icon: FileText },
+  // { id: "notes", label: "Notes", icon: FileText },
   { id: "links", label: "Liens", icon: ExternalLink },
   { id: "calendars", label: "Calendriers", icon: Calendar1 },
   { id: "communication", label: "Mails", icon: Mail },
   { id: "cekilui", label: "Cékilui", icon: Users },
-  { id: "pokemon", label: "Pokémon", icon: Gamepad2 },
+  // { id: "pokemon", label: "Pokémon", icon: Gamepad2 },
   // { id: "bibli", label: "Bibli", icon: LibraryBig },
 ];
